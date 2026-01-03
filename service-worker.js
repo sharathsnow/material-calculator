03e40d6b6094e2b317fb991297bf85173bb25d13
Sharath Chandra .T <51002962+sharathsnow@users.noreply.github.com>
2026-01-03 19:23:01 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "solar-calculator-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./IMG_4454.jpeg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }) 
+  );
+});
